2525981413d7af1fa45d5b97716959f813d1b326
xc-kiwiberry <xc_kiwiberry@qq.com>
2017-08-15 12:46:48 +0800

    mid

--- runMiddle.sh
+++ runMiddle.sh
@@ -5,7 +5,7 @@
  -t ~/data_set/dev_test/nist03040508/nist03040508.cn \
  -tl ~/data_set/dev_test/nist03040508/nist03040508.en0 \
  -d ~/data_set/dev_test/nist06/nist06.zh \
- -dl ~/data_set/dev_test/nist06/nist06.en0 \
+ -dl ~/data_set/dev_test/nist06/nist06.en \
  --input_size 62  --hidden_size 100  --batch_size 80 \
  --num_layers 1  --num_epochs -1
-
+ --save_freq 100 --print_freq 50 \ 
