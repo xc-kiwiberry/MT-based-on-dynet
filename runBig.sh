3537faf4913bac730f10adb3b5c20c634663d487
xc-kiwiberry <xc_kiwiberry@qq.com>
2017-08-22 15:12:42 +0800

    freq

--- runBig.sh
+++ runBig.sh
@@ -8,4 +8,4 @@
  -dl ~/data_set/dev_test/nist06/nist06.en \
  --input_size 620  --hidden_size 1000  --batch_size 80  \
  --num_layers 1  -num_epochs -1 \
- --save_freq 2000 --print_freq 500 \ 
+ --save_freq 10 --print_freq 10 \ 
