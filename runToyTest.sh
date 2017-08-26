ee96218c399ab06e653a990e58c2c6558384e83b
xc-kiwiberry <xc_kiwiberry@qq.com>
2017-08-26 20:44:28 +0800

    toytest

--- runToyTest.sh
+++ runToyTest.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 ./mt_test.exe \
- --dynet-mem 2300 \
+ --dynet-devices CPU \
+ --dynet-mem 1024 \
  --name toytest \
- -m old_models//big_13_1_620_1000_tloss=3344.99_BLEU=35.68.params \
- -t ~/data_set/train/train.zh \
- -tl ~/data_set/train/train.en \
- -ts ~/data_set/toyData/toy.zh \
- -tsl ~/data_set/toyData/toy.en \
- --input_size 620 --hidden_size 1000 --num_layers 1 \
+ -m toy_1_60_100.params \
+ -t ~/data_set/toyData/toy.zh \
+ -tl ~/data_set/toyData/toy.en \
+ -ts ~/data_set/toyData/test.zh \
+ -tsl ~/data_set/toyData/test.en \
+ --input_size 60 --hidden_size 100 --num_layers 1 \
  --debug_info 0 \
