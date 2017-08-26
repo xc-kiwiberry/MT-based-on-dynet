75bb7c0510569c4eeefa7b15541a903654efec41
xc-kiwiberry <xc_kiwiberry@qq.com>
2017-08-26 16:52:59 +0800

    minor

--- runTest.sh
+++ runTest.sh
@@ -2,11 +2,10 @@
 ./mt_test.exe \
  --dynet-mem 2300 \
  --name nist06 \
- -m old_models//big_20_1_620_1000_tloss=3305.22_dloss=59.9371.params \
+ -m old_models//big_13_1_620_1000_tloss=3344.99_BLEU=35.68.params \
  -t ~/data_set/train/train.zh \
  -tl ~/data_set/train/train.en \
  -ts ~/data_set/dev_test/nist06/nist06.zh \
  -tsl ~/data_set/dev_test/nist06/nist06.en \
  --input_size 620 --hidden_size 1000 --num_layers 1 \
  --debug_info 0 \
- 
--- runToyTest.sh
+++ runToyTest.sh
@@ -2,11 +2,10 @@
 ./mt_test.exe \
  --dynet-mem 2300 \
  --name toytest \
- -m old_models//big_20_1_620_1000_tloss=3305.22_dloss=59.9371.params \
+ -m old_models//big_13_1_620_1000_tloss=3344.99_BLEU=35.68.params \
  -t ~/data_set/train/train.zh \
  -tl ~/data_set/train/train.en \
  -ts ~/data_set/toyData/toy.zh \
  -tsl ~/data_set/toyData/toy.en \
  --input_size 620 --hidden_size 1000 --num_layers 1 \
  --debug_info 0 \
- 
