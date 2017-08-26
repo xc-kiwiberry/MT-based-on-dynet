ad191526de5bb128a339e82abdc99852e2529960
xc-kiwiberry <xc_kiwiberry@qq.com>
2017-08-26 20:47:23 +0800

    bug

--- runToyTest.sh
+++ runToyTest.sh
@@ -3,7 +3,7 @@
  --dynet-devices CPU \
  --dynet-mem 1024 \
  --name toytest \
- -m toy_1_60_100.params \
+ -m models//toy_1_60_100.params \
  -t ~/data_set/toyData/toy.zh \
  -tl ~/data_set/toyData/toy.en \
  -ts ~/data_set/toyData/test.zh \
