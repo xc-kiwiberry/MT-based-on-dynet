5e463419b177f506f8dae9430af08d0a3a00d545
xc-kiwiberry <xc_kiwiberry@qq.com>
2017-09-19 15:10:03 +0800

    update compile

--- compile.sh
+++ compile.sh
@@ -3,16 +3,16 @@ g++ my_train_encdec.cc \
 -o mt_gpu.exe \
 -ldynet \
 -std=c++11 \
--L/global-mt/huangxuancheng/projects/dynet/build/dynet
+-L/home/huangxuancheng/dynet/build/dynet
 
 g++ my_train_encdec.cc \
 -o mt_cpu.exe \
 -ldynet \
 -std=c++11 \
--L/global-mt/huangxuancheng/projects/dynet/build/dynet
+-L/home/huangxuancheng/dynet/build/dynet
 
 g++ my_test.cc \
 -o mt_test.exe \
 -ldynet \
 -std=c++11 \
--L/global-mt/huangxuancheng/projects/dynet/build/dynet
+-L/home/huangxuancheng/dynet/build/dynet
