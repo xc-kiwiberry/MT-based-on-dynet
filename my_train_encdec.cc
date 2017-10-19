72f2ae5287c2d9f128481413643ad5529e33eb2d
xc-kiwiberry <xc_kiwiberry@qq.com>
2017-10-19 21:41:16 +0800

    4

--- my_train_encdec.cc
+++ my_train_encdec.cc
@@ -81,7 +81,7 @@ int main(int argc, char** argv) {
   // Fetch program specific parameters (see ../utils/cl-args.h) ------------------------------------
   //Params params;
   get_args(argc, argv, params, TRAIN);
-  cerr << "mrt_enable="<<mrt_enable<<endl;
+  cerr << "mrt_enable="<<params.mrt_enable<<endl;
   if (0 != params.mrt_enable) {
     cerr << "Training criteria is MRT." << endl;
     params.BATCH_SIZE = 1;
