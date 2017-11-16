33941c75af3beac9b596ed232b10b765bac3db82
xc-kiwiberry <xc_kiwiberry@qq.com>
2017-11-16 21:35:06 +0800

    bug 5

--- compile.sh
+++ compile.sh
@@ -3,10 +3,10 @@ g++ my_train_encdec.cc \
 -o mt_train \
 -ldynet \
 -std=c++11 \
--L~/dynet/build/dynet
+-L/data/disk2/private/hxc/dynet/build/dynet
 
 g++ my_test.cc \
 -o mt_test \
 -ldynet \
 -std=c++11 \
--L~/dynet/build/dynet
+-L/data/disk2/private/hxc/dynet/build/dynet
